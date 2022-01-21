01fa4e2898fb6ddbad223784388b5f3bda4dce15
Andrew Read <aj.read13@gmail.com>
2022-01-21 18:44:40 -0500

    working on routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,13 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  Category.findAll({
+    include:  [
+     { model: Product,
+     attributes: ["id", 'product_name', "price", "stock", 'category_id']
+     }]
+  })
   // find all categories
   // be sure to include its associated Products
 });
