a48e8c6e27bc395f32bd756c0ee995e2fe9486da
Andrew Read <aj.read13@gmail.com>
2022-01-23 13:32:52 -0500

    finished product model

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes, DECIMAL } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -15,21 +15,25 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-      product_name: {
-        type: DataTypes.STRING,
-        allowNull: false,       
-      },
-        price: {
-          type: DataTypes.DECIMAL,
-          allowNull: true,
-        },
-        category_id: {
-          type: DataTypes.INTEGER,
-          foreign
-        }
-      
-    
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
